d980a6614a06daec5565003f14eb4689e761fa76
chaserChoi <chlwogur2010@gmail.com>
2023-11-05 23:28:32 +0900

    Chapter 05 수정

--- src/components/Ad.jsx
+++ src/components/Ad.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Ad = ({ show }) => {
   return show ? (
-    <img src="img/movieAd.png" alt="Ad" style={{ width: "100%" }} />
+    <img src="./movieAd.png" alt="Ad" style={{ width: "100%" }} />
   ) : null;
 };
 
